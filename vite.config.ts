71890569be0058ace41258b79ba0acd81b20aaad
xiangxiang023 <yixiang023@163.com>
2026-01-07 15:16:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
       // 这里的配置决定了 App 安装到手机上的样子
       manifest: {
         name: 'My OOTD Note', // 手机上显示的全名
-        short_name: 'OOTE Note', // 图标下的短名字
+        short_name: 'OOTD Note', // 图标下的短名字
         description: '记录衣橱与心情日记的个人应用',
         theme_color: '#ffffff', // 状态栏颜色
         background_color: '#ffffff', // 启动页背景色
